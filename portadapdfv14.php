9a4d6dab749feedb396f969882545ec497744971
Arnau <arnaucat@gmail.com>
2012-05-22 22:39:52 +0200

    Modificacions project 2nd iteration
    Modificacions dels errors i bugs esmentats

--- portadapdfv14.php
+++ portadapdfv14.php
@@ -5,8 +5,6 @@ header("Cache-Control: no-cache, must-revalidate");                   // HTTP/1.
 header("Pragma: no-cache");
 include('class.ezpdf.php');
 
-
-
 $pdf =& new Cezpdf('a4');
 $pdf->selectFont('./fonts/Helvetica');
 
@@ -15,19 +13,11 @@ $any2=date("y");   /*any petit 09*/
 $mes=date("m");
 $dia=date("d");
 $diaris = array("lavanguardia","abc","elpais","elpunt_avui","elmundo","elperiodico_cat","larazon","publico","eleconomista","expansion","5dias","sport","marca","mundodeportivo","as","heraldo_aragon");
-$urldiaris=array("http://www.lamanyana.cat/diari/" . $any2 . $mes . $dia . "max.jpg","http://www.segre.com/fileadmin/PortadaDiariSegre/PortadaSegreCatala.jpg");
-$temps = array("http://qras.es/tallaimg/tavui.jpg","http://www.aemet.es/imagenes_d/eltiempo/prediccion/espa/" . $any1 . $mes . $dia . "0012_p01wesp0.jpg");
-
-$ruta1="hola";
 
 
 $k=0;
-$n1=sizeof($diaris);
+$i=0;
 $n2=sizeof($urldiaris);
-$n4=sizeof($temps);
-$rutestemps=array();
-$urlbones = array();
-$n3=0;
 
 function image_exist1($url) {
 	$AgetHeaders = @get_headers($url);
@@ -37,7 +27,7 @@ function image_exist1($url) {
 	$n3++;
 	}
 }
-$i=0;
+
 while($i<$n1)
   {
   $url="http://img.kiosko.net/" . $any1 . "/". $mes ."/" . $dia . "/es/" . $diaris[$i] .".750.jpg";
@@ -67,26 +57,7 @@ while($j<$n2){
 	}
 }
 
-while($k<$n4){
-	$file = $temps[$k];
-	$AgetHeaders = @get_headers($url);
-	if (preg_match("|200|", $AgetHeaders[0])) {
-	$rutestemps[]=$file;
-	$k++;
-	} else {
-	$n3++;
-	$k++;
-	}
-}
-
 foreach ($urlbones as $valor) {
-   
-   echo "url: ". $valor. "<br />";
-  
-  }
-  echo "Total portades: ".sizeof($urlbones). "<br />";
-
-  foreach ($urlbones as $valor) {
    $pdf->ezText("\n",15);
    $pdf->ezImage($valor,2,490,'none');
    $pdf->ezText("\n",10);
@@ -94,19 +65,9 @@ foreach ($urlbones as $valor) {
    $pdf->ezNewPage();
    }
    
-  foreach ($rutestemps as $valor2) {
-    $pdf->ezText("\n",9);
-	$pdf->ezImage($valor2,0,352,'center');
-	$pdf->ezText("\n",10);
-   } 
-   	$pdf->ezImage("upload/uploads/img.jpg", 0, 'none', 'full', 'center');
-	$pdfcode = $pdf->ezOutput();
-	$fp=fopen('./docs/'. $any1 . '-'. $mes .'-' . $dia .'-pro.pdf','wb');
 
 fwrite($fp,$pdfcode);
 fclose($fp);
-echo "Les portades ja estan creades";
-echo "\n";
 echo '<a href="http://www.qras.es/adminportimail/pro/docs/'. $any1 . '-'. $mes .'-' . $dia .'-pro.pdf">Clica per veure les portades</a>';
 
  
