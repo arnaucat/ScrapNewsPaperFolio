39d36ad5958172b8d1af4f9c2db02a52a502b64a
Arnau <arnaucat@gmail.com>
2012-05-22 22:20:31 +0200

    He afegit el php jUnit FirtsTest.php
    Per fer les test unitaris

---
+++ FirstTest.php
@@ -0,0 +1,24 @@
+<?php
+ 
+class FirstTest extends PHPUnit_Framework_TestCase {
+    protected $fixture;
+ 
+    protected function setUp() {
+        // Create the Array fixture.
+        $this->fixture = Array();
+    }
+ 
+    public function testNewArrayIsEmpty() {
+        // Assert that the size of the Array fixture is 0.
+        $this->assertEquals(0, sizeof($this->fixture));
+    }
+ 
+    public function testArrayContainsAnElement() {
+        // Add an element to the Array fixture.
+        $this->fixture[] = ‘Element’;
+ 
+        // Assert that the size of the Array fixture is 1.
+        $this->assertEquals(1, sizeof($this->fixture));
+    }
+}
+?>
